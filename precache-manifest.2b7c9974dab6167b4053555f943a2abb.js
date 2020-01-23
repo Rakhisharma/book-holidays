a5a03e113c21439c004b997d7557ae8b248dd4db
Rakhi Sharma <rakhish1994@gmail.com>
2020-01-23 21:34:38 +0100

    Updates

---
+++ precache-manifest.2b7c9974dab6167b4053555f943a2abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be7caa54a5a2100c05302671a64481f",
+    "url": "/book-holidays/index.html"
+  },
+  {
+    "revision": "8eb55e412a9cfe68fa72",
+    "url": "/book-holidays/static/css/main.32bac123.chunk.css"
+  },
+  {
+    "revision": "a8850449b470e2b19fda",
+    "url": "/book-holidays/static/js/2.48feab59.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/book-holidays/static/js/2.48feab59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8eb55e412a9cfe68fa72",
+    "url": "/book-holidays/static/js/main.a7623c9a.chunk.js"
+  },
+  {
+    "revision": "e9c324580bb79a71ec76",
+    "url": "/book-holidays/static/js/runtime-main.fb15fd07.js"
+  }
+]);
